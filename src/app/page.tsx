1c9b147af825c7273cd76e65bf879fc43f5bcc7d
Batissta <francinaldofilho05@gmail.com>
2025-03-26 22:13:42 -0300

    styles: editor page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import EditorBase from "./components/editor/Editor";
 
 export default function Home() {
   return (
-    <main className="bg-zinc-950 h-screen">
+    <main className="bg-zinc-950 min-h-screen">
       <EditorBase />
     </main>
   );
