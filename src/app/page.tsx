c14dd509c5db31b342a75829a2714254a5c319fb
Batissta <francinaldofilho05@gmail.com>
2025-03-26 21:26:16 -0300

    feat: creating a next project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import EditorBase from "./components/editor/Editor";
+
+export default function Home() {
+  return (
+    <main className="bg-zinc-950 h-screen">
+      <EditorBase />
+    </main>
+  );
+}
