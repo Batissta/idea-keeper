9612d8e3ae23e8282e8a995180a309736f9013ed
Batissta <francinaldofilho05@gmail.com>
2025-03-27 20:46:26 -0300

    feat: creating an initialContent for editor

---
+++ src/components/editor/InitialContent.ts
@@ -0,0 +1,7 @@
+export const initialContent = `
+<h1>Bem-vindo ao Meu Site</h1>
+<h2>Uma pequena introdução</h2>
+<p>Este é um exemplo básico de HTML. Aqui você pode encontrar algumas tags simples sendo usadas para criar uma estrutura de página.</p>
+<p>Você pode adicionar mais conteúdo, como textos, imagens ou links, conforme sua necessidade!</p>
+<a href="https://linkedin.com/in/francinaldobatista">Tipo esse link aqui!</a>
+`;
